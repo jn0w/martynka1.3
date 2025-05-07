db89e00fffff1b1c9ea1a67beb347a14f0bcdf76
jn0w <B00153676@mytudublin.ie>
2025-05-07 22:56:46 +0100

    deleted

--- nourishmate/src/app/components/ClientProviders.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Toaster } from "react-hot-toast";
-
-export default function ClientProviders() {
-  // ClientProviders component: sets up client-side utilities for the app
-  // Uses Toaster from react-hot-toast to display notifications
-  return <Toaster position="bottom-right" />;
-}
